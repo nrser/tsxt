09603a793bfd0838c008fcc97232bde6febcc30e
nrser <neil@neilsouza.com>
2019-12-06 05:07:21 -0800

    Fix that damn test

--- __tests__/map.tests.tsx
+++ __tests__/map.tests.tsx
@@ -1,41 +1,40 @@
 /* @jsx Tsxt */
 
-import 'jest';
-import 'jest-extended';
-import _ from 'lodash/fp';
-
-import './test_helpers';
+import "jest";
+import "jest-extended";
+import _ from "lodash/fp";
 
 import Tsxt from '../lib';
-import Map from  '../lib/map';
+import Map from '../lib/map';
+import './test_helpers';
 
-describe( `Map helper`, () => {
-  it( `works..?`, () => {
+describe(`Map helper`, () => {
+  it(`works..?`, () => {
     const obj = {
-      x: 'ex',
-      y: 'why?',
-      z: 'zee! zee! zee!',
+      x: "ex",
+      y: "why?",
+      z: "zee! zee! zee!",
     };
-    
+
     const md = Tsxt.md(
-      <document>
+      <div>
         <p>Here's my list:</p>
-        
+
         <ol>
-          <Map object={ obj }>
-            <li>{ (k: any, v: any) => `${ k } => ${ v }` }</li>
+          <Map object={obj}>
+            <li>{(k: any, v: any) => `${k} => ${v}`}</li>
           </Map>
         </ol>
-      </document>
+      </div>,
     );
-    
-    expect( md  ).toEqualLines(
-        `Here's my list:`,
-        ``,
-        `1.  x => ex`,
-        `2.  y => why?`,
-        `3.  z => zee! zee! zee!`
+
+    expect(md).toEqualLines(
+      `Here's my list:`,
+      ``,
+      `1.  x => ex`,
+      `2.  y => why?`,
+      `3.  z => zee! zee! zee!`,
     );
-    
-  })  
+
+  })
 });
