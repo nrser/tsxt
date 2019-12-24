f4fa96a4be54f6c8ac86a8efffc7e8a110a9e3e5
nrser <neil@neilsouza.com>
2019-12-24 16:43:48 -0500

    Fix the `print` module declaration

--- include/print.d.ts
+++ include/print.d.ts
@@ -2,6 +2,20 @@
  * Type declarations for [print][] package.
  * 
  * [print]: https://www.npmjs.com/package/print
+ * 
+ * Used [@types/wordwrap][] as a reference to get the "exports a function with
+ * added properties" right, which I did not find intuitive.
+ * 
+ * [@types/wordwrap]: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/wordwrap/index.d.ts
  */
 
-declare module 'print';
+declare module 'print' {
+  
+  function print(x: any): string;
+  
+  namespace print {
+    export function out(x: any): void;
+  }
+  
+  export = print;
+}
